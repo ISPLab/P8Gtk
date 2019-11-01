53a54348dab97cc4cdc0346fff94d0550dca436a
Andrey <for_orlov@mail.ru>
2019-11-01 15:15:50 +0300

    init

---
+++ GtkXamarinSkia/Program.cs
@@ -0,0 +1,27 @@
+﻿using SkiaTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace GtkXamarinSkia
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+             Gtk.Application.Init();
+             Forms.Init();
+             var app = new App();
+             var window = new FormsWindow();
+             window.LoadApplication(app);
+             window.SetApplicationTitle("This is app from GTK");
+             window.Show();
+             Gtk.Application.Run();
+        }
+    }
+}
---
+++ SkiaTest/App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SkiaTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainPage = new ContentPage();
+
+        }
+    
+
+
+
+    }
+}
