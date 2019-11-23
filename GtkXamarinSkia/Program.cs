e7e5828e979c62d3d9ae263502fc5362edf0b359
ISPLab <for_orlov@mail.ru>
2019-11-23 12:05:19 +0300

    run gtk window test

--- GtkXamarinSkia/Program.cs
+++ GtkXamarinSkia/Program.cs
@@ -14,14 +14,49 @@ namespace GtkXamarinSkia
         [STAThread]
         public static void Main(string[] args)
         {
-             Gtk.Application.Init();
-             Forms.Init();
-             var app = new App();
-             var window = new FormsWindow();
-             window.LoadApplication(app);
-             window.SetApplicationTitle("This is app from GTK");
-             window.Show();
-             Gtk.Application.Run();
+            RunGtkWindow();
+        }
+
+
+        static void RunXamarinWidow()
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("This is app from GTK");
+            window.Show();
+            Gtk.Application.Run();
+        }
+
+        static void RunGtkWindow()
+        {
+            Gtk.Application.Init();
+            Gtk.Window myWin = new Gtk.Window("My first GTK# Application! ");
+            myWin.Resize(200, 200);
+            myWin.ButtonPressEvent += (s,ev)=>
+            {
+
+            };
+            //Create a label and put some text in it.
+
+            Gtk.Label myLabel = new Gtk.Label();
+            myLabel.Text = "Hello World!!!!";
+
+            //Add the label to the form
+            // myWin.Add(myLabel);
+
+            Gtk.Button gtk_but = new Gtk.Button();
+            gtk_but.HeightRequest = 100;
+            gtk_but.WidthRequest = 100;
+            gtk_but.Label = "button";
+            myWin.Add(gtk_but);
+            //Show Everything
+
+            myWin.ShowAll();
+
+            Gtk.Application.Run();
         }
     }
 }
--- GtkXamarinSkia/SKGtkViewRenderer.cs
+++ GtkXamarinSkia/SKGtkViewRenderer.cs
@@ -11,12 +11,13 @@ namespace GtkXamarinSkia.GTK
 {
     public class SKGtkViewRenderer : ViewRenderer<PureSkia, SKWidget>
     {
+        
         protected override void Dispose(bool disposing)
         {
             var control = Control;
             if (control != null)
             {
-                control.PaintSurface -= OnPaintSurface;
+               // control.PaintSurface -= OnPaintSurface;
             }
             base.Dispose(disposing);
         }
@@ -32,13 +33,36 @@ namespace GtkXamarinSkia.GTK
                 // create the native view
                 if (Control == null)
                 {
+                   
                     var view = CreateNativeControl();
+                    view.HeightRequest = 200;
+                    view.WidthRequest = 200;
+                  
                     view.PaintSurface += OnPaintSurface;
                     SetNativeControl(view);
+                    view.ButtonPressEvent += View_ButtonPressEvent;
+                    view.FocusMoved += View_FocusMoved;
+                    view.EnterNotifyEvent += View_EnterNotifyEvent;
+                  
                 }
                 (e.NewElement as SkiaTest.ISKCanvasViewController).SurfaceInvalidated += HandleSurfaceInvalidated;
             }
             base.OnElementChanged(e);
+        }
+
+        private void View_EnterNotifyEvent(object o, Gtk.EnterNotifyEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void View_FocusMoved(object o, Gtk.FocusMovedArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void View_ButtonPressEvent(object o, Gtk.ButtonPressEventArgs args)
+        {
+            //throw new NotImplementedException();
         }
 
         void OnPaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs e)
